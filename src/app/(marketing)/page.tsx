0905217e95939a8ddd4d45d1fd868505ff2dfc33
Jorge Matos <jisapjisap@gmail.com>
2024-08-05 18:07:33 +0200

    pricing

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -159,6 +159,22 @@ const HomePage = () => {
             </Container>
          </Wrapper>
 
+         {/* Pricing */}
+         <Wrapper className="flex flex-col items-center justify-center py-12 relative">
+            <div className="hidden md:block absolute top-0 -right-1/3 w-72 h-72 bg-blue-500 rounded-full blur-[10rem] -z-10"></div>
+            <Container>
+               <div className="max-w-md mx-auto text-start md:text-center">
+                  <SectionBadge title="Pricing" />
+                  <h2 className="text-3xl lg:text-4xl font-semibold mt-6">
+                     Unlock the right plan for your business
+                  </h2>
+                  <p className="text-muted-foreground mt-6">
+                     Choose the best plan for your business and start building your dream website today
+                  </p>
+               </div>
+            </Container>
+         </Wrapper>
+
       </section>
    )
 };
